3c17a58e76407741f22f5a306f80017ae57da584
Isaac Richter <isaac.richter@rochester.edu>
2024-10-07 14:30:45 -0400

    separate integer/float/double toolchains

--- toolchain32d.cmake
+++ toolchain32d.cmake
@@ -1,4 +1,4 @@
 set(RISCV_XLEN 32)
-set(RISCV_EXTENSIONS m;a;c)
+set(RISCV_EXTENSIONS m;a;f;d;c)
 
 include("${CMAKE_CURRENT_LIST_DIR}/toolchain_common.cmake")
--- toolchain64.cmake
+++ toolchain64.cmake
@@ -1,4 +1,4 @@
 set(RISCV_XLEN 64)
-set(RISCV_EXTENSIONS m;a;f;d;c)
+set(RISCV_EXTENSIONS m;a;c)
 
 include("${CMAKE_CURRENT_LIST_DIR}/toolchain_common.cmake")
